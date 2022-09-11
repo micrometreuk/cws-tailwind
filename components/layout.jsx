d636c9ccd45218867e7a21319c8123d398b1ef90
Henok <henokmicrometre@gmail.com>
2022-09-11 16:59:57 +0000

    tailwind added + heading

---
+++ components/layout.jsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export const siteTitle = "Croydon Web Services";
+
+export default function Layout({ children }) {
+  return (
+    <div className="section">
+      <Head>
+        <link rel="icon" href="/img/logo.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#ffffff"/>
+        <meta
+          name="description"
+          content="Croydon and surrounding areas."
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta
+          name="Croydon Web Services"
+          content={siteTitle}
+        />
+        <title>{siteTitle}</title>
+      </Head>
+      <main>{children}</main>
+    </div>
+  );
+}
