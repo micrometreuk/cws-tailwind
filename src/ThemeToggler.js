05766fd55797865a4024fa894ca2c887dce753d5
Henok <henokmicrometre@gmai.com>
2022-10-08 13:50:14 +0100

    removing test

--- src/ThemeToggler.js
+++ src/ThemeToggler.js
@@ -8,12 +8,12 @@ const ThemeToggler = () => {
   if (!mounted) return null;
   return (
     <button
-      className="w-8 h-8 bg-blue-100 rounded-lg dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
+      className="w-8 h-8 bg-blue-100 rounded-lg border border-gray-200 dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       aria-label="Toggle Dark Mode"
     >
       {theme === 'light' ? (
-        <MoonIcon className="text-blue-500 w-5 h-5" />
+        <MoonIcon className="text-blue-500 w-5 h-5 " />
       ) : (
         <SunIcon className="text-blue-400 w-5 h-5" />
       )}
